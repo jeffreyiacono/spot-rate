33b6dab7be9b81021ee92ddaa95d603c7778fa28
Jeff Iacono <iacono@squareup.com>
2013-05-04 18:41:30 -0700

    Goodbye parens

--- lib/spot-rate.rb
+++ lib/spot-rate.rb
@@ -13,7 +13,7 @@ class SpotRate
     self.available_converters[converter_key.to_sym] = converter_class
   end
 
-  def self.[](hash)
+  def self.[] hash
     raise ArgumentError unless hash.size <= 2
 
     new(from_currency: hash.keys.first, to_currency: hash.values.first)
@@ -21,15 +21,15 @@ class SpotRate
       .spot_rate
   end
 
-  def initialize(config = {})
+  def initialize config = {}
     @from_currency = config[:from_currency]
     @to_currency   = config[:to_currency]
   end
 
-  def use(requested_converter_key)
+  def use requested_converter_key
     self.class
         .available_converters[requested_converter_key.to_sym]
-        .new(@from_currency, @to_currency)
+        .new @from_currency, @to_currency
   end
 
   def spot_rate
@@ -37,7 +37,7 @@ class SpotRate
   end
 
   def default_currency_converter
-    use(:default)
+    use :default
   end
 end
 
